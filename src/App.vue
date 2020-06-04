a95327ddf578774badcd1f8fa6c541714a0ce28b
Philip Broadway <me@philipbroadway.com>
2020-06-04 11:20:59 -0700

    minor styling and nav

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,15 @@
 <template>
   <div id="app">
-    <b-nav vertical class="w-25">
-      <b-nav-item active>
-        <router-link to="/">Home</router-link>
-      </b-nav-item>
-      <b-nav-item>
-        <router-link to="/components/button">Button</router-link>
-      </b-nav-item>
-      <b-nav-item>
-        <router-link to="/components/input">Input</router-link>
-      </b-nav-item>
-    </b-nav>
+    <navigation />
     <router-view />
   </div>
 </template>
 
 <script>
-import { BNav, BNavItem } from "bootstrap-vue";
-
+import Navigation from "@/views/Navigation.vue";
 export default {
   name: "App",
-  components: {
-    BNav,
-    BNavItem
-  }
+  components: { Navigation }
 };
 </script>
 
---
+++ src/views/Navigation.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-navbar toggleable="lg" type="light" class="mb-2">
+    <b-navbar-brand href="#">ct-component-library</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item-dropdown text="Components" right>
+          <b-dropdown-item href="#">
+            <router-link to="/components/button">Button</router-link>
+          </b-dropdown-item>
+          <b-dropdown-item href="#">
+            <router-link to="/components/input">Input</router-link>
+          </b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+import {
+  BNavbarNav,
+  BNavbar,
+  BNavItemDropdown,
+  BDropdownItem,
+  BCollapse,
+  BNavbarToggle,
+  BNavbarBrand
+} from "bootstrap-vue";
+export default {
+  name: "Navigation",
+  components: {
+    BNavbarNav,
+    BNavbar,
+    BNavItemDropdown,
+    BDropdownItem,
+    BCollapse,
+    BNavbarToggle,
+    BNavbarBrand
+  }
+};
+</script>
