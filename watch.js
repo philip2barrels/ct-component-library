08bd9d4bd469b9bd44e489044d33be10e11853a4
Philip Broadway <me@philipbroadway.com>
2020-06-08 09:11:42 -0700

    refine reloading

--- watch.js
+++ watch.js
@@ -1,20 +1,8 @@
 var chokidar = require('chokidar')
 const shell = require('shelljs')
 
-chokidar
-  .watch('.', {
-    ignored: [
-      /node_modules/,
-      /public/,
-      /tests/,
-      /src\/assets/,
-      /src\/plugins/,
-      /src\/router/,
-      /src\/views/,
-      /\.git/,
-    ],
-  })
-  .on('all', (event, path) => {
-    console.log(`[changed] ${path}`)
-    shell.exec('./copy-assets.sh')
-  })
+chokidar.watch('src/components').on('all', (event, path) => {
+  console.log(`[changed] ${path}`)
+  shell.exec('./copy-assets.sh')
+  shell.exec('node build-sidebar.js')
+})
